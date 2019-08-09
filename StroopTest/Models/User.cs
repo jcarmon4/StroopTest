7ca30b13ceefca5e745479b5e711742d53758481
Alejocram <jcarmon4@eafit.edu.co>
2019-08-08 22:38:08 -0500

    Updating name of fields, and changing the routing link of Edit page.

--- StroopTest/Models/User.cs
+++ StroopTest/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,11 @@ namespace StroopTest.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        [Display(Name = "Last name")]
         public string Lastname { get; set; }
         public string Gener { get; set; }
         public int Age { get; set; }
+        [Display(Name = "Register date")]
         [DataType(DataType.DateTime)]
         public DateTime RegisterDate { get; set; }
     }
